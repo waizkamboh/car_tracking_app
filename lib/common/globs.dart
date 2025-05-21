dab2f48e14382828fa0120bc8ffa4f1891140e5e
waizkamboh <waizkamboh6@gmail.com>
2025-05-21 10:43:50 +0500

    create globs class and add constant

---
+++ lib/common/globs.dart
@@ -0,0 +1,37 @@
+
+import 'package:car_tracking_app/main.dart';
+
+class Globs {
+  static const appName = "Car Tracking";
+
+  static void udStringSet(String data, String key) {
+    prefs?.setString( key, data );
+  }
+
+  static String udValueString(String key) {
+    return prefs?.getString(key ) ?? "";
+  }
+}
+
+class SVKey {
+  static const mainUrl = "http://localhost:3001";
+  static const baseUrl = "$mainUrl/api/";
+  static const nodeUrl = mainUrl;
+
+  // static const nvCarJoin = "car_join";
+  // static const nvCarUpdateLocation = "car_update_location";
+  //
+  // static const svCarJoin = "$baseUrl$nvCarJoin";
+  // static const svCarUpdateLocation = "$baseUrl$nvCarUpdateLocation";
+}
+
+class KKey {
+  static const payload = "payload";
+  static const status = "status";
+  static const message = "message";
+}
+
+class MSG {
+  static const success = "success";
+  static const fail = "fail";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:car_tracking_app/screen/map_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+SharedPreferences? prefs;
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
   runApp(const MyApp());
 }
 
